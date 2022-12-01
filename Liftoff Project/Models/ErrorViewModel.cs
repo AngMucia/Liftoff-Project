849b2b355b43ce6cdc5a8e08d7320db87328a15f
Angel Mucia <angelmucia7@gmail.com>
2022-12-01 16:19:45 -0600

    connected to mysql

---
+++ Liftoff Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Liftoff_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
