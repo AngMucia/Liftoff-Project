01d90c39b01a64504234b6a06cc03cb8533c64c0
Angel Mucia <angelmucia7@gmail.com>
2022-12-05 15:08:05 -0600

    Added api json info as classes for matches, teamstandings, and teams

--- Liftoff Project/Models/Match.cs
+++ Liftoff Project/Models/Match.cs
@@ -2,5 +2,31 @@
 {
     public class Match
     {
+
+        public class Rootobject
+        {
+            public int id { get; set; }
+            public string type { get; set; }
+            public string group { get; set; }
+            public string home_team_id { get; set; }
+            public string away_team_id { get; set; }
+            public int home_score { get; set; }
+            public int away_score { get; set; }
+            public object home_scorers { get; set; }
+            public object away_scorers { get; set; }
+            public string persian_date { get; set; }
+            public string local_date { get; set; }
+            public string stadium_id { get; set; }
+            public string time_elapsed { get; set; }
+            public string finished { get; set; }
+            public int matchday { get; set; }
+            public string home_team_fa { get; set; }
+            public string away_team_fa { get; set; }
+            public string home_team_en { get; set; }
+            public string away_team_en { get; set; }
+            public string home_flag { get; set; }
+            public string away_flag { get; set; }
+        }
+
     }
 }
--- Liftoff Project/Models/TeamStandings.cs
+++ Liftoff Project/Models/TeamStandings.cs
@@ -2,5 +2,20 @@
 {
     public class TeamStandings
     {
+
+            public string team_id { get; set; }
+            public string mp { get; set; }
+            public string w { get; set; }
+            public string l { get; set; }
+            public string pts { get; set; }
+            public string gf { get; set; }
+            public string ga { get; set; }
+            public string gd { get; set; }
+            public string d { get; set; }
+            public string name_fa { get; set; }
+            public string name_en { get; set; }
+            public string flag { get; set; }
+        
+
     }
 }
