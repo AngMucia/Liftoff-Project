4eeaca366d85813f6ffba471003ea16a47de3ef9
Angel Mucia <angelmucia7@gmail.com>
2022-12-06 21:56:02 -0600

    Made a searchbar to pick out one country

--- Liftoff Project/Controllers/HomeController.cs
+++ Liftoff Project/Controllers/HomeController.cs
@@ -28,11 +28,11 @@ namespace Liftoff_Project.Controllers
         private string baseUrl = "http://api.cup2022.ir/api/v1/";
         private string bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzg5MTNlNWZhNzhmOWNkZjQxMzg2ODEiLCJpYXQiOjE2NzAzNjMzNzUsImV4cCI6MTY3MDQ0OTc3NX0.3xBsgPsUHwgWHhrmUc05CJ5dJ3wwB1kT9Ukix2gSLh8";
 
-
+        IList<Team> teams = new List<Team>();
 
         public async Task<IActionResult> Index()
         {
-            IList<Team>teams = new List<Team>();
+            //IList<Team>teams = new List<Team>();
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri(baseUrl);
@@ -67,12 +67,50 @@ namespace Liftoff_Project.Controllers
         }
 
         [HttpPost]
-        public IActionResult SearchTeam()
+        public async Task<IActionResult> SearchTeam(string searchItem)
         {
-            return View();
-        }
-        
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + bearerToken);
+                HttpResponseMessage getData = await client.GetAsync("team");
 
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(results);
+                    RootObject data = JsonConvert.DeserializeObject<RootObject>(results);
+                    teams = data.Data;
+
+                }
+                else
+                {
+
+                    Console.WriteLine(getData.StatusCode);
+                }
+
+                List<Team> team = new List<Team>();
+                for (int i = 0; i < teams.Count; i++)
+                {
+                    if (searchItem.Equals(teams[i].Name_en))
+                    {
+                        team.Add(teams[i]);
+                        break;
+                    }
+                   
+
+                }
+                if(team.Count == 0)
+                {
+                    ViewBag.error = "This is not an option";
+                }
+                ViewData.Model = team;
+                return View("Index");
+            }
+
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
