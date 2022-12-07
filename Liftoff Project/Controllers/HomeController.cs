b4240f3364e4c337b0615f7ab9231ea86cde50bb
Angel Mucia <angelmucia7@gmail.com>
2022-12-06 18:58:18 -0600

    Connected api to project, made a new model to handle list of teams, and updated home/index.cshtml to show list teams.

--- Liftoff Project/Controllers/HomeController.cs
+++ Liftoff Project/Controllers/HomeController.cs
@@ -1,11 +1,19 @@
 ﻿using Liftoff_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using static Liftoff_Project.Models.Match;
 
 namespace Liftoff_Project.Controllers
 {
@@ -17,9 +25,39 @@ namespace Liftoff_Project.Controllers
         {
             _logger = logger;
         }
+        private string baseUrl = "http://api.cup2022.ir/api/v1/";
+        private string bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzg5MTNlNWZhNzhmOWNkZjQxMzg2ODEiLCJpYXQiOjE2NzAzNjMzNzUsImV4cCI6MTY3MDQ0OTc3NX0.3xBsgPsUHwgWHhrmUc05CJ5dJ3wwB1kT9Ukix2gSLh8";
 
-        public IActionResult Index()
+
+
+        public async Task<IActionResult> Index()
         {
+            IList<Team>teams = new List<Team>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + bearerToken);
+                HttpResponseMessage getData = await client.GetAsync("team");
+               
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(results);
+                    RootObject data = JsonConvert.DeserializeObject<RootObject>(results);
+                    teams = data.Data;
+
+                }
+                else
+                {
+                    
+                    Console.WriteLine(getData.StatusCode);
+                }
+                ViewData.Model = teams;
+                   
+            }
+            
             return View();
         }
 
---
+++ Liftoff Project/Models/RootObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Liftoff_Project.Models
+{
+    public class RootObject
+    {
+        public string Status { get; set; }
+        public List<Team> Data { get; set; }
+    }
+}
