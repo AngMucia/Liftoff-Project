aba287b47d32c41fc832f374592390d6517f0973
Angel Mucia <angelmucia7@gmail.com>
2022-12-09 19:53:01 -0600

    Updated the bracket controller so we can receive team data and update drop downmenu for brackets

--- Liftoff Project/ViewModels/AddBracketViewModel.cs
+++ Liftoff Project/ViewModels/AddBracketViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace Liftoff_Project.ViewModels
+﻿using Liftoff_Project.Models;
+using System.Collections.Generic;
+
+namespace Liftoff_Project.ViewModels
 {
     public class AddBracketViewModel
     {
+        public User User { get; set; }
+        public string UserId { get; set; }
+        /*public List<Team> Teams { get; set; }
+        public string TeamId { get; set; }*/
+        public Bracket Brackets { get; set; }
+        public int BracketId { get; set;}
 
+        public AddBracketViewModel(User user, List<Bracket> brackets)
+        {
+            
+            User = user;
+        }
     }
 }
