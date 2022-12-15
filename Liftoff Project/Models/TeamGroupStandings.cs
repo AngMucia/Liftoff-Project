a1bb32f7b63b3c1afcacb8762cabc1ce808cc820
zkrem56 <zkremer702@gmail.com>
2022-12-15 13:42:27 -0600

    User is able to view team standings

---
+++ Liftoff Project/Models/TeamGroupStandings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Liftoff_Project.Models
+{
+    public class TeamGroupStandings
+    {
+        public string _id { get; set; }
+        public string Group { get; set; }
+        public List<TeamStandings> Teams;
+    }
+}
